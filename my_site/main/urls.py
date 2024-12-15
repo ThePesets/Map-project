13ffa4bf638e3da4fdcedd28664c71bb7a2865e6
ThePesets <tretyakova.vs.12@gmail.com>
2024-12-15 03:44:02 +0300

    Add files via upload

--- my_site/main/urls.py
+++ my_site/main/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'), 
+    path('info1', views.info1, name='full_info1'),
+    path('info2', views.info2, name='full_info2'),
 ]
--- my_site/main/views.py
+++ my_site/main/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'main/index.html')
+def info1(request):
+    return render(request, 'main/full_info1.html')
+def info2(request):
+    return render(request, 'main/full_info2.html')
