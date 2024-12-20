3fc415f51fa5d32d432a518cedd711314101e70e
ThePesets <tretyakova.vs.12@gmail.com>
2024-12-20 19:53:07 +0300

    Add files via upload

--- my_site/main/views.py
+++ my_site/main/views.py
@@ -6,3 +6,35 @@ def info1(request):
     return render(request, 'main/full_info1.html')
 def info2(request):
     return render(request, 'main/full_info2.html')
+def info3(request):
+    return render(request, 'main/full_info3.html')
+def info4(request):
+    return render(request, 'main/full_info4.html')
+def info5(request):
+    return render(request, 'main/full_info5.html')
+def info6(request):
+    return render(request, 'main/full_info6.html')
+def info7(request):
+    return render(request, 'main/full_info7.html')
+def info8(request):
+    return render(request, 'main/full_info8.html')
+def info9(request):
+    return render(request, 'main/full_info9.html')
+def info10(request):
+    return render(request, 'main/full_info10.html')
+def info11(request):
+    return render(request, 'main/full_info11.html')
+def info12(request):
+    return render(request, 'main/full_info12.html')
+def info13(request):
+    return render(request, 'main/full_info13.html')
+def info14(request):
+    return render(request, 'main/full_info14.html')
+def info15(request):
+    return render(request, 'main/full_info15.html')
+def info16(request):
+    return render(request, 'main/full_info16.html')
+def info17(request):
+    return render(request, 'main/full_info17.html')
+def info18(request):
+    return render(request, 'main/full_info18.html')
