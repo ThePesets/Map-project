6deaaeefaa743b9dff44a1c497ca84a36cb73048
ThePesets <tretyakova.vs.12@gmail.com>
2024-12-21 23:42:59 +0300

    Delete views.py

--- views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def page1(request):
-    return render(request, "main/page1.html")
-def page2(request):
-    return render(request, "main/page2.html")
-def page3(request):
-    return render(request, "main/page3.html")
